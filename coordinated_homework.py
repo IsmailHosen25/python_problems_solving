b4e8a1d0e40450d1f0e7cf8fd5bb1ddfbab62a64
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-06-06 23:50:22 +0600

    ccorrdinated homework

---
+++ coordinated_homework.py
@@ -0,0 +1,67 @@
+### Q-1
+
+# def print_triangle(num , chr):
+    
+#     for i in range(1,num+1):
+#         for j in range(1,i+1):
+#             print(chr,end=" ")
+#         print()
+# print_triangle(10,"*")
+
+
+
+### Q-2
+
+# def volumn(r):
+#     v=(4/3)*3.14*(r)**3
+#     return v
+# result=volumn(5.3)
+# print("%.3f"%result)
+
+
+
+#### Q-3
+
+# def factorial (n):
+#     result=1
+#     for i in range(n,0,-1):
+#        result=result*i
+#     return result
+# result=factorial(5)
+# print(result)
+
+
+# ### Q-4
+
+# def number_of_digits(n):
+     
+#      num=n
+#      total_digits=0
+#      while num != 0:
+#           num=num//10
+#           total_digits=total_digits+1
+#      return total_digits
+
+# result=number_of_digits(12345)
+# print(result)
+
+
+###Q-5
+
+# def sum_of_digits(n):
+#     sum=0
+#     num=n
+#     while num != 0:
+#        sum=sum+num%10
+#        num=num//10
+#     return sum
+
+# result=sum_of_digits(968)
+# print(result)
+
+
+###Q-6
+
+
+def reverse_number (n):
+    print(n)
