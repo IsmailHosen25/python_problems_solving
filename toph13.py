7dfd6d3c7acefb13a131c33edd58530a5cb90dcc
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-07-11 17:37:46 +0600

    toph 13 added

---
+++ toph13.py
@@ -0,0 +1,25 @@
+def check(x):
+    if len(x)%2!=0: return 0
+    if x=='': return 1
+
+    try:
+        for i in range(len(x)):
+            if i == len(x)-1:break
+            if (x[i]=='(' and x[i+1]==')') or (x[i]=='{' and x[i+1]=='}') or (x[i]=='[' and x[i+1]==']'):
+                y=x[:i]
+                z=x[i+2:]
+                x=y+z
+                if check(x)==1:
+                    return 1
+    except:
+        return 0
+    return 0
+
+x = input()
+if len(x)<=25:
+    if x=="":
+        print("No")
+    elif check(x)==1:
+        print("Yes")
+    else:
+        print("No")
