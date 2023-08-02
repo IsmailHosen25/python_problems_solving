ed534f9018ecf5e6052893f0a6b140b6deb4af49
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-08-02 19:13:05 +0600

    toph13 modidied

--- toph13.py
+++ toph13.py
@@ -1,10 +1,13 @@
 def check(x):
-    if len(x)%2!=0: return 0
-    if x=='': return 1
+    if len(x)%2!=0:
+        return 0
+    if x=='':
+        return 1
 
     try:
         for i in range(len(x)):
-            if i == len(x)-1:break
+            if i == len(x)-1:
+                break
             if (x[i]=='(' and x[i+1]==')') or (x[i]=='{' and x[i+1]=='}') or (x[i]=='[' and x[i+1]==']'):
                 y=x[:i]
                 z=x[i+2:]
